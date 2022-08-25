be74514d4d962209bdf53496b6d513768a6e9de4
matheusbrito07 <matheusrj.dev@gmail.com>
2022-08-25 09:39:15 -0300

    Componente grid and valueBox

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
-
+import ValueBox from '../common/widget/valueBox'
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <ContentHeader title='Dashboard' small='Versão legacy'/>
         <Content>
-          Dashboard
+          <ValueBox 
+          cols='12 4' 
+          color='green' 
+          icon='bank' 
+          value='R$ 10'
+          text='Total de Credit'/>
+          <ValueBox 
+          cols='12 4' 
+          color='red' 
+          icon='credit-card' 
+          value='R$ 10'
+          text='Total de Debit'/>
+          <ValueBox 
+          cols='12 4' 
+          color='blue' 
+          icon='money' 
+          value='R$ 10'
+          text='Valor Consolidado'/>
         </Content>
       </div>
     )
