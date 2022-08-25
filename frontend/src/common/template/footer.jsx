dceb05597e69aed8250285e20fad648bdb9adb17
matheusbrito07 <matheusrj.dev@gmail.com>
2022-08-25 07:56:35 -0300

    Criando ambiente do frontend, instalando dependencias e configurando webpack com dev e production e tambem criando os components header,nav e footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+    BEFORE US, THERE WAS NOTHING. BEFORE NOTHING, THERE WERE MONSTERS. 
+      <a href="https://leagueoflegends.fandom.com/wiki/Zed/LoL/Audio#Galaxy_Slayer"> Zed</a>
+    </strong>
+  </footer>
+)
