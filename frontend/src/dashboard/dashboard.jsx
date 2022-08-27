6f628a9a654935a356be1f4d17764c8449b612be
matheusbrito07 <matheusrj.dev@gmail.com>
2022-08-27 19:23:50 -0300

    create row

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,30 +2,33 @@ import React, { Component } from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
+import Row from '../common/template/layout/row'
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <ContentHeader title='Dashboard' small='Versão legacy'/>
         <Content>
-          <ValueBox 
-          cols='12 4' 
-          color='green' 
-          icon='bank' 
-          value='R$ 10'
-          text='Total de Credit'/>
-          <ValueBox 
-          cols='12 4' 
-          color='red' 
-          icon='credit-card' 
-          value='R$ 10'
-          text='Total de Debit'/>
-          <ValueBox 
-          cols='12 4' 
-          color='blue' 
-          icon='money' 
-          value='R$ 10'
-          text='Valor Consolidado'/>
+          <Row>
+            <ValueBox 
+            cols='12 4' 
+            color='green' 
+            icon='bank' 
+            value='R$ 10'
+            text='Total de Credit'/>
+            <ValueBox 
+            cols='12 4' 
+            color='red' 
+            icon='credit-card' 
+            value='R$ 10'
+            text='Total de Debit'/>
+            <ValueBox 
+            cols='12 4' 
+            color='blue' 
+            icon='money' 
+            value='R$ 10'
+            text='Valor Consolidado'/>
+          </Row>
         </Content>
       </div>
     )
